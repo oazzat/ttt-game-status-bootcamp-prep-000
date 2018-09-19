b27b8493e69c30ef43d0442845bc2917fa7128fe
Omar A <oazzat19@gmail.com>
2018-09-19 16:29:57 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -90,6 +90,8 @@ end
 def over?(board)
   if won?(board) == true || draw?(board) == true || full?(board) == true
     return true
+  elsif won?(board) == true && full?(board) == false
+    return true
   else
     return false
   end
